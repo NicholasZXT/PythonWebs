1c9ed8e73a6aacebdad416932fe9ad98e58b8fc5
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2023-07-26 16:05:46 +0800

    Django初始化+练习

---
+++ HelloDjango/ideablog/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideablog.settings')
+env_profile = os.environ.get('BLOG_PROFILE', 'dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'ideablog.settings.{env_profile}')
+
+application = get_wsgi_application()
