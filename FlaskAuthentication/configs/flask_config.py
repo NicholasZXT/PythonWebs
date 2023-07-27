dc3d59cc0cac333ac671310ffb9bbbaa26aa03e6
NicholasZXT <1036562425@qq.com>
2023-07-27 15:10:32 +0800

    Flask-HttpAuth认证练习

--- FlaskAuthentication/configs/flask_config.py
+++ FlaskAuthentication/configs/flask_config.py
@@ -7,6 +7,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # 用于生成token的KEY
+    SECRET_KEY = 'flask-insecure-nv6-(i1-659xonvcxe&luz90!jsp0ag!y7lt0_8-01al#iilw2'
+    # 认证用户
+    AUTHORIZED_USER = {'yourself': 'f**k'}
+    # 过期时间，单位s
+    TOKEN_EXPIRATION = 60 * 5
 
 class DevelopmentConfig(BaseConfig):
     mysql_conf = {
