36193c979991dceb7cbcc43abeec12fdce5fbdb8
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2023-08-01 17:06:47 +0800

    Django-ORM 复习

--- HelloDjango/blog/models.py
+++ HelloDjango/blog/models.py
@@ -12,6 +12,8 @@ class Category(models.Model):
         (STATUS_DELETE, '删除'),
     }
     # 定义字段
+    # 默认下，如果用户没有定义主键，Django会使用类似下面的语句创建一个自增主键
+    # id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEM, verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
@@ -22,8 +24,8 @@ class Category(models.Model):
     class Meta:
         managed = True   # 是否交由 ORM 框架管理，默认为True
         # db_tablespace = 'dbbase'   # 数据库
-        db_table = 'category'                 # 设置表名称，默认下Django会以全小写的 {app_name}_{model_class_name} 形式创建表
-        db_table_comment = '存放文章类别'       # 设置表的注释
+        db_table = 'blog_category'           # 设置表名称，默认下Django会以全小写的 {app_name}_{model_class_name} 形式创建表
+        # db_table_comment = '存放文章类别'    # 设置表的注释, 4.2版本开始才有这个功能
         verbose_name = '类别'                 # 此Model的文本表示，用于在 Admin 界面显示等
         verbose_name_plural = verbose_name   # 复数形式，默认下会在 verbose_name 后面加上字符串 s 表示复数（比如在Admin界面显示时）
         ordering = ['name', '-status']       # 指定数据记录的排序字段，默认ASC，- 表示 DESC
@@ -69,8 +71,8 @@ class Post(models.Model):
     content_html = models.TextField(verbose_name="正文html代码", blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_md = models.BooleanField(default=False, verbose_name="markdown语法")
-    category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.DO_NOTHING)
     tag = models.ManyToManyField(Tag, verbose_name="标签")
+    category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.DO_NOTHING)
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
