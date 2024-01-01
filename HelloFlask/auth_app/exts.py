680026f4e1b4f8d3c60a46ecbbeed722268a7a44
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2024-01-02 00:44:12 +0800

    Flask-JWT插件研究

--- HelloFlask/auth_app/exts.py
+++ HelloFlask/auth_app/exts.py
@@ -7,6 +7,7 @@ from flask_httpauth import HTTPTokenAuth
 from werkzeug.http import HTTP_STATUS_CODES
 # itsdangerous 的 TimedJSONWebSignatureSerializer 只在 2.0.1 及其之前的版本中有，2.x 开始的官方文档建议转向 authlib
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+from flask_jwt_extended import JWTManager
 from HelloFlask.extensions import getLogger
 from auth_app.models import User
 
@@ -19,7 +20,10 @@ login_manager = LoginManager()
 # Web-API的接口认证
 auth = HTTPTokenAuth(scheme='Bearer')
 
-# --------------------- Flask-Login 的回调函数 ---------------------------------
+# Web-API的JWT认证
+jwt = JWTManager()
+
+# --------------------- Flask-Login 的hook函数 ---------------------------------
 # 必须要在 login_manager 中注册一个 user_loader 函数，用于配合 Flask-Login 提供的 current_user 使用
 # 如果用户已登录，current_user 会调用此处返回加载的 User 类对象；
 # 如果用户未登录，则返回 Flask-Login 内置的 AnonymousUserMixin 类对象
@@ -34,7 +38,9 @@ def load_user(uid):
 # 如果不设置这个，访问需要登录的资源时，会返回 401 Unauthorized 的简单HTML页面
 login_manager.login_view = "login_bp.to_login"
 
-# -------------------- Flask-HttpAuth 的回调函数 --------------------------------
+# -------------------- Flask-HttpAuth 的hook函数 --------------------------------
+# Flask-HttpAuth 只是封装了各类认证方案（HTTPBasicAuth, HTTPTokenAuth等）的流程框架，但是流程中的具体细节，比如token生成，token验证
+# 等逻辑的实现，需要我们手动在下面的hook函数中实现
 @auth.verify_token
 def verify_token(token):
     """
@@ -115,3 +121,40 @@ def api_abort(code, message=None, **kwargs):
     response = jsonify(code=code, message=message, **kwargs)
     response.status_code = code
     return response
+
+# -------------------- Flask-JWT-Extended 的hook函数 --------------------------------
+# 相比于Flask-HttpAuth，Flask-JWT-Extended只提供基于token的认证，它不仅实现了认证的框架流程，还帮我们实现了上述token生成、token认证的过程
+# 使用起来更方便，因此 Flask-JWT-Extended 需要实现的hook函数比较少
+
+# 第一个要实现的hook函数是从用户对象中获取用户ID，用于区分用户，后续用来生成token
+# @jwt.user_identity_loader
+def get_user_id(user):
+    # 传入的参数user是我们自己定义的User模型对象，需要返回的是用户ID字段的值
+    return user.uid
+
+# 第2个hook函数用于数据库中获取当前用户的记录
+# @jwt.user_lookup_loader
+def user_lookup_callback(jwt_header, jwt_data):
+    """
+    这个回调函数必须接受两个参数，然后根据这两个参数附带的JWT信息，解析出用户信息并返回（可以是任何Python对象）.
+    返回值后续可以通过两种方式访问到：
+      1. flask_jwt_extended.current_user 的属性
+      2. flask_jwt_extended.get_current_user() 方法
+    :param jwt_header: 第1个参数是一个dict，存放了JWT的 header 数据
+    :param jwt_data: 第2个参数也是一个dict，存放了JWT的 payload 数据
+    :return: 要么是包含用户信息的 任意Python对象，要么是 None
+    """
+    identity = jwt_data["uid"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+# 下面是不需要访问数据库的版本，用于快速验证
+@jwt.user_identity_loader
+def get_user_id_mock(user):
+    return user['username']
+
+# 第2个hook函数用于数据库中获取当前用户的记录
+@jwt.user_lookup_loader
+def user_lookup_callback_mock(jwt_header, jwt_data):
+    identity = jwt_data["username"]
+    user_config = current_app.config['AUTHORIZED_USERS'].get(identity, None)
+    return user_config
--- HelloFlask/configs.py
+++ HelloFlask/configs.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from urllib import parse
+from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,10 +10,17 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # 用于生成token的KEY
     SECRET_KEY = 'flask-insecure-nv6-(i1-659xonvcxe&luz90!jsp0ag!y7lt0_8-01al#iilw2'
+    # JWT使用的秘钥
+    JWT_SECRET_KEY = 'flask-insecure-nv6-(i1-659xonvcxe&luz90!jsp0ag!y7lt0_8-01al#iilw2jwt'
+    # JWT的token刷新时间设置
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=8)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+    # JWT设置请求中可以存放JWT信息的位置
+    JWT_TOKEN_LOCATION = ["headers"]
     # 认证用户，附带了用户的角色信息
     AUTHORIZED_USERS = {
-        'admin': {'passwd': 'admin', 'roles': ['admin']},
-        'yourself': {'passwd': 'people', 'roles': ['others']},
+        'admin': {'passwd': 'admin', 'uid': 1, 'username': 'admin', 'roles': ['admin']},
+        'yourself': {'passwd': 'people', 'uid': 2, 'username': 'yourself', 'roles': ['others']},
     }
     # 过期时间，单位s
     TOKEN_EXPIRATION = 60 * 20
