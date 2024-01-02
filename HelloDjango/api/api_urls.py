2cc8d3208aa22be0ca07a9de7fce4bd505ccd9c6
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2024-01-02 16:16:49 +0800

    复习DRF的身份认证+权限控制

--- HelloDjango/api/api_urls.py
+++ HelloDjango/api/api_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path, include
 from .views import get_student, list_student, create_student, TeacherApiView, TeacherGenericView, \
-    TeacherCompositeView, TeacherViewSet, create_draft_user, DraftOpenView, DraftAuthView, DraftOwnerView
+    TeacherCompositeView, TeacherViewSet, create_draft_user, DraftOpenView, DraftAuthView, DraftOwnerView, DraftJwtView
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # ViewSet 需要使用 Router 来集成
@@ -10,6 +11,7 @@ router = DefaultRouter()
 router.register(r'teacher/viewset', viewset=TeacherViewSet)
 
 urlpatterns = [
+    # ------- 基于函数的视图 ---------
     path('get_student/<int:sid>', get_student),
     path('list_student', list_student),
     path('create_student', create_student),
@@ -19,18 +21,22 @@ urlpatterns = [
     # 其他class-based view
     path('teacher/genericviews/<int:tid>', TeacherGenericView.as_view()),
     path('teacher/compositeviews', TeacherCompositeView.as_view()),
-    # Draft相关视图
-    path('create_draft_user', create_draft_user),
-    path('draft/open/<int:nid>', DraftOpenView.as_view()),
-    path('draft/auth/<int:nid>', DraftAuthView.as_view()),
-    path('draft/owner/<int:nid>', DraftOwnerView.as_view()),
 
-    # 引入 DRF 的用户登录界面视图函数，具体URL为 api-auth/login
+    # 引入DRF的用户登录界面视图函数，具体URL为 api/auth/login
     path('auth/', include('rest_framework.urls')),
+    # 引入DRF里 rest_framework.authtoken 应用提供的token获取视图函数
+    # path('auth/static/token', obtain_auth_token),
 
     # DRF-simple-jwt 提供的用于获取 token 和 刷新token 的视图函数
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Draft相关视图，展示DRF里的 身份认证 + 权限控制
+    path('create_draft_user', create_draft_user),
+    path('draft/open/<int:nid>', DraftOpenView.as_view()),
+    path('draft/auth/<int:nid>', DraftAuthView.as_view()),
+    path('draft/owner/<int:nid>', DraftOwnerView.as_view()),
+    path('draft/jwt/<int:nid>', DraftJwtView.as_view()),
 ]
 
 # 默认下，DRF 框架的 Response 对象会对接口返回的数据使用默认的HTML页面进行渲染，稍微封装一下，容易查看数据
