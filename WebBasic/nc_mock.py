39dd9837e286c1c4f7f4b975f1c385e4d72758f4
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2025-04-05 15:31:24 +0800

    nc工具-新增信号停止功能

--- WebBasic/nc_mock.py
+++ WebBasic/nc_mock.py
@@ -3,8 +3,10 @@
 模拟 nc 命令向服务器发送消息，用于测试 Spark Streaming 和 Flink
 """
 from typing import Generator
+import sys
 import os
 import socket
+import signal
 from time import sleep
 
 def nc_client(server_ip, server_port, messages):
@@ -22,6 +24,10 @@ def nc_client(server_ip, server_port, messages):
             sock.sendall(message.encode('utf-8'))
 
 
+def signal_handler(sig, frame):
+    print("\n收到 Ctrl+C，正在退出服务器...")
+    sys.exit(0)
+
 def nc_server(host: str, port: int, messages: Generator[str, None, None]):
     # 创建一个TCP/IP套接字
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,27 +36,42 @@ def nc_server(host: str, port: int, messages: Generator[str, None, None]):
     # 监听传入连接
     server_socket.listen(1)
     print(f"服务器正在监听 {host}:{port}...")
-    while True:
-        # 等待连接
-        client_socket, client_address = server_socket.accept()
-        try:
-            print(f"连接来自: {client_address}, 准备发送数据")
-            for message in messages:
-                print(f"Sending >>> : {message}", end='')
-                client_socket.sendall(message.encode('utf-8'))
-            print(f"Sending message done.")
-            # 接收数据
-            # while True:
-            #     data = client_socket.recv(1024)
-            #     if data:
-            #         print(f"收到数据: {data.decode('utf-8')}")
-            #         # 发送数据回客户端
-            #         client_socket.sendall(data)
-            #     else:
-            #         break
-        finally:
-            # 清理连接
-            client_socket.close()
+    # 设置信号处理函数
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    try:
+        while True:
+            # 等待连接
+            try:
+                client_socket, client_address = server_socket.accept()
+            except KeyboardInterrupt as e:
+                break
+            try:
+                print(f"连接来自: {client_address}, 准备发送数据")
+                for message in messages:
+                    print(f"Sending >>> : {message}", end='')
+                    client_socket.sendall(message.encode('utf-8'))
+                print(f"Sending message done.")
+                # 接收数据
+                # while True:
+                #     data = client_socket.recv(1024)
+                #     if data:
+                #         print(f"收到数据: {data.decode('utf-8')}")
+                #         # 发送数据回客户端
+                #         client_socket.sendall(data)
+                #     else:
+                #         break
+            except KeyboardInterrupt as e:
+                print(f"收到 Ctrl+C，正在退出客户端...")
+                # client_socket.close()
+                break
+            finally:
+                # 清理连接
+                client_socket.close()
+    except KeyboardInterrupt as e:
+        print(f"收到 Ctrl+C，正在退出服务器...")
+    finally:
+        server_socket.close()
 
 
 def read_file(file_path: str, time_separator='>>>batch', interval_seconds=2, encoding='utf-8') -> Generator[str, None, None]:
@@ -80,5 +101,5 @@ if __name__ == "__main__":
     nc_server(server_ip, server_port, content)
 
     # 作为客户端
-    nc_client(server_ip, server_port, content)
+    # nc_client(server_ip, server_port, content)
 
