8f4edcf4e777ccdce221298a774e4f7bc4cdbad6
NicholasZXT <1036562425@qq.com>
2025-11-21 00:19:27 +0800

    Litestar研究：与FastAPI依赖注入系统对比总结

--- HelloLitestar/api/__init__.py
+++ HelloLitestar/api/__init__.py
@@ -1,2 +1,3 @@
 from .views_func import api_router
-from .views_controller import MyController, UserController, DependencyController
+from .views_controller import MyController, UserController
+from .views_dependency import DependencyController
