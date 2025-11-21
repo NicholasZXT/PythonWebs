e5a2186ea62d476debdc9379aafacde7485a0608
NicholasZXT <1036562425@qq.com>
2025-11-21 18:21:14 +0800

    Litestar认证&鉴权研究整理

--- HelloLitestar/auth/auth_middlewares.py
+++
@@ -1,9 +0,0 @@
-"""
-Litestar 提供的鉴权机制也是采用中间件形式来实现的。
-"""
-from litestar.enums import ScopeType
-from litestar.middleware import AbstractAuthenticationMiddleware, AuthenticationResult
-from litestar.datastructures import MutableScopeHeaders
-from litestar.types import ASGIApp, Message, Receive, Scope, Send
-from litestar.connection import ASGIConnection
-from litestar.security import AbstractSecurityConfig
